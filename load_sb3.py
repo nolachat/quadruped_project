7ab971adb242c85956e3eca4bc0d10abf9558a82
Paul Lanfermann <paul.lanfermann@epfl.ch>
2023-12-14 16:13:57 +0100

    test1

--- load_sb3.py
+++ load_sb3.py
@@ -58,7 +58,7 @@ from utils.file_utils import get_latest_model, load_all_results
 LEARNING_ALG = "PPO"
 interm_dir = "./logs/intermediate_models/"
 # path to saved models, i.e. interm_dir + '121321105810'
-log_dir = interm_dir + '121423142514'
+log_dir = interm_dir + '121423154534'
 
 # initialize env configs (render at test time)
 # check ideal conditions, as well as robustness to UNSEEN noise during training
