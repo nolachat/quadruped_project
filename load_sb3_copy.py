038df6515b2b9c99564f6d8ee6387e70556a3077
Paul Lanfermann <paul.lanfermann@epfl.ch>
2024-01-10 12:07:32 +0100

    del

--- load_sb3_copy.py
+++ load_sb3_copy.py
@@ -62,7 +62,7 @@ LEARNING_ALG = "PPO"
 interm_dir = "./logs/intermediate_models/"
 # path to saved models, i.e. interm_dir + '121321105810'
 # log_dir = interm_dir + '121523095438'
-log_dir = interm_dir + 'CPG_desireless_v2_Goal_4'
+log_dir = interm_dir + 'CPG_RL'
 
 # initialize env configs (render at test time)
 # check ideal conditions, as well as robustness to UNSEEN noise during training
